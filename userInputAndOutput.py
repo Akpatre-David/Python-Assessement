624a0bbbc5e3edf31342744b65d29177e603607e
Akpatre-David <akpatredavid17@gmail.com>
2025-03-09 19:30:39 +0100

    final-commit

--- userInputAndOutput.py
+++ userInputAndOutput.py
@@ -1,3 +1,3 @@
-favorite_color= input("Enter your favouirite color: ")
+favorite_color= input("Enter your favorite color: ")
 
 print(f"your favorite  color is {favorite_color}. Thats a great choice!")
